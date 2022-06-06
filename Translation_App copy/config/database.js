605562000db0403fa6a087de51eef0df1d42d286
Daniela Morin <dmorin@wellesley.edu>
2022-06-06 00:33:59 -0400

    Delete Translation_App copy directory

--- Translation_App copy/config/database.js
+++
@@ -1,6 +0,0 @@
-// config/database.js
-module.exports = {
-
-    'url' : 'mongodb+srv://dmorin2020:rHMaqBHnjjjPVffd@cluster0.aub17.mongodb.net/translation?retryWrites=true&w=majority', 
-    'dbName': 'translation'
-};
